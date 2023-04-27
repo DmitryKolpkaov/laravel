869ceea5ea8e951450c980414af99e71d6c446f1
Dmitriy Kolpakov <111364017+DmitryKolpkaov@users.noreply.github.com>
2023-04-27 12:00:50 +0600

    Потренировал методы

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,37 @@ class PostController extends Controller
 {
     public function index()
     {
-        $post = Post::find(1);
-        dd($post);
+//        $posts = Post::all();
+//        $data = [];
+//        foreach($posts as $post) {
+//            $data[] = [
+//                dump($post->title)
+//            ];
+//        }
+//
+//        dd($posts);
+
+//        $posts = Post::where('is_published', 1)->get();
+//        $data = [];
+//        foreach($posts as $post){
+//            $data[] = [
+//                'id' => $post->id,
+//                'title' => $post->title
+//            ];
+//        }
+//
+//        dd($data);
+
+//        $post = Post::where('id', 1)->get()[0];
+//        $data[] = [
+//            'id' => $post->id,
+//            'title' => $post->title,
+//            'content' => $post->content
+//        ];
+//
+//        dd($data);
+
+        $post = Post::where('is_published', 1)->first();
+        dump ($post->title);
     }
 }
