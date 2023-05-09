11956194e3e93d2a8aaf2d09eacabe870eb3012e
Dmitriy Kolpakov <111364017+DmitryKolpkaov@users.noreply.github.com>
2023-05-09 12:26:28 +0600

    Сильнее погрузился во view, сделал шаблон main

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Post</title>
-</head>
-<body>
+@extends('layouts.main')
+@section('content')
+    <h1>This is posts page</h1>
     <div>
         @foreach($posts as $post)
             <div>{{$post->title}}</div>
+            <div>{{$post->content}}</div>
+            <div>{{$post->likes}}</div>
         @endforeach
     </div>
-</body>
-</html>
+@endsection
